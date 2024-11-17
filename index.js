ee8d4e91f074d4d4641a2f397f83e07efaad09a3
SilverPsalm <171736682+silverpsalm@users.noreply.github.com>
2024-11-17 12:00:33 -0500

    Remove flex-basis from .square class, assign in JS
    Remove flex-basis from .square css so that the
    class could be assigned to user-generated squares,
    too. Flex-basis set for initial squares. Now when
    new squares are generated, .square's styling does
    not need to be repeated, only flex-basis.

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
 const container = document.querySelector("#container");
 
 // generate 16 squares in container
-for (i = 0; i<16 ;i++) {
-const square = document.createElement("div");
-square.className = "square";
-container.appendChild(square);
+for (i = 0; i < 16; i++) {
+    const square = document.createElement("div");
+    square.className = "square";
+    square.style.flexBasis = "25%";
+    container.appendChild(square);
 }
 
-container.addEventListener("mouseover",(event) =>{
+container.addEventListener("mouseover", (event) => {
     // change color
     console.log(event.target.id);
     if (event.target.id != "container") {
-    event.target.classList.add("active");
+        event.target.classList.add("active");
     }
 
-    
+
 })
 
-container.addEventListener("mouseout",(event) => {
+container.addEventListener("mouseout", (event) => {
     // change color back on mouse leave
     console.log(event.target.classList);
-    if(event.target.id != "container") {
+    if (event.target.id != "container") {
         event.target.classList.remove("active");
     }
 })
 
 const btn = document.querySelector("button");
 
-btn.addEventListener("click",(e) => {
+btn.addEventListener("click", (e) => {
 
     console.log(e);
 
     // button event listener
-    let sideCount = prompt("How many squares per side?","(max 100)");
+    let sideCount = prompt("How many squares per side?", "(max 100)");
     let squareTotal = sideCount * sideCount; // total square #
     let squarePercent = 100 / sideCount; // Flex Basis value for new squares
-    // delete current squares in grid
-    container.innerHTML = '';
-    // const currentSquares = document.querySelectorAll(".square");
+    container.innerHTML = '';     // delete current squares in grid
     // set new square size based on percentage
-
-
+    let newSquare = document.createElement("div");
 })
